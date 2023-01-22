6262ae6d30d6d19d469055eb5bff57d74f9c4617
Den <selection4test@gmail.com>
2023-01-22 21:24:51 +0300

    css extra fix

--- src/css-build-tool.js
+++ src/css-build-tool.js
@@ -2,4 +2,7 @@ import './css/styles.css'
 import './css/sweetalert2@11.6.16.min.css'
 // Others...
 
+// NOTE: Or this:
+// import './css/_prod-2023.bundle.css'
+
 console.log('High level styles: All old css imported for main css bundle...')
